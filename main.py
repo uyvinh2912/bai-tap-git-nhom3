80af2942ceac56e3d06bb7d8153f3499a549154a
NganThiNguyen <nguyennganthi207@gmail.com>
2026-06-17 17:16:09 +0700

    feat: dev A tinh thue co giam tru gia canh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def tinh_thue_thu_nhap(thu_nhap):
-    # TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
-    return 0
-
-print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
+    # Dev A áp dụng giảm trừ 4 trieu va thue 5%
+    thu_nhap_tinh_thue = thu_nhap - 4000000
+    return max(0, thu_nhap_tinh_thue * 0.05)
